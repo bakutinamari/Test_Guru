07c98cb3ab6399bf870cad6fdff447f9979de1f3
aleksandr <bakutina.mari@gmail.com>
2022-02-03 21:48:28 +0100

    db

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,7 @@
 class Test < ApplicationRecord
   belongs_to :category
+
+  def self.name_tests(title)
+    where(categories: {title: title}).order(title: :desc)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+users = User.create([{name: 'Alex', age: 23},
+	                 {name: 'Dmitry', age: 45}, 
+	                 {name: 'Pavel', age: 15}])
+
+categories = Category.create([{title: 'Geografy'},
+                              {title: 'History'},
+                              {title: 'Matematika'}])
+
+tests = Test.create([{title: 'Test 1', level: 2, category: categories.first},
+                     {title: 'Test 2', level: 3, category: categories.last}
+                     {title: 'Test 3', level: 2, category: categories[1]}
+                     {title: 'Test 4', level: 3, category: categories.last}
+                     {title: 'Test 5', level: 1, category: categories.first}
+                   ])
