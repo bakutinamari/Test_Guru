404f0a882b62340df428fc6754b1fcc43d4e1abd
aleksandr <bakutina.mari@gmail.com>
2022-02-06 21:59:41 +0100

    new1

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,12 @@
 class Test < ApplicationRecord
   belongs_to :category
 
-  
+  attr_accessor :level, :title
+
+  def initialize(level, title)
+    @level = level
+    @title = title
+  end
 
   def self.sort_title_tests(category)
     Test.where(category_id == category.id).order(id: :desc).pluck(:title) 
