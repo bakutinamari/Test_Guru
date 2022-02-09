c854708703e0b1e0821601c5c4af06bb8b6b2552
aleksandr <bakutina.mari@gmail.com>
2022-02-10 00:21:18 +0100

    new3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 	
 	def tests_by_level(levels)
-       self.joins("LEFT JOIN user_tests ON tests.id = test_id").where("tests.level = ?,user_tests.user_id = ?", levels, 2)
+       self.joins("LEFT JOIN user_tests ON tests.id = test_id").where("tests.level = ?,user_tests.user_id = ?", levels, self.id)
 	end
        
 end
---
+++ app/models/user_test.rb
@@ -0,0 +1,4 @@
+class UserTest < ApplicationRecord
+  belongs_to :test_id
+  belongs_to :user_id
+end
