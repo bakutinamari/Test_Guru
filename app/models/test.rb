fad8b9ddf079c266c207ef39088668dcbea7169b
aleksandr <bakutina.mari@gmail.com>
2022-02-15 10:41:03 +0100

    new commit

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,9 @@
 class Test < ApplicationRecord
   belongs_to :category
+  belongs_to :author
+  has_many :questions
+  has_many :user_tests
+  has_many :users, through :user_tests
 
   def self.sort_title_tests(category)
     Test.joins("LEFT JOIN categories ON tests.category_id = categories.id")
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	has_many :user_tests
+	has_many :tests, through :user_tests
 	def tests_by_level(levels)
 		self.joins("LEFT JOIN user_tests ON tests.id = test_id").where("tests.level = ?, user_tests.user_id = ?", levels, self.id)
 	end
