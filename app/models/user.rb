223a9aa7f71dd07a86ab5edd278f58e5b37a32c4
aleksandr <bakutina.mari@gmail.com>
2022-02-21 19:56:03 +0100

    changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :tests, class_name: "Test", foreign_key: :author_id
+  has_many :created_tests, class_name: "Test", foreign_key: :author_id
   has_many :user_tests, dependent: :destroy
   has_many :tests, through :user_tests
 	
