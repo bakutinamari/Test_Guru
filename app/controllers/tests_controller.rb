764196302826c3bb3eb59d6f85927a50da45d8b5
aleksandr <bakutina.mari@gmail.com>
2022-04-08 10:50:43 +0200

    addes

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,23 @@
 class TestsController < ApplicationController
   def index
-  	@test = Test.all
+    @questions = Question.all
+  end
+
+  def show
+    @test = Test.find(params[:id])
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(params[:test])
+
+    if test.save
+      redirect_to :@tests
+    else 
+      render :new
+    end
   end
 end
