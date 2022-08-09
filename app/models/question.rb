1ea7172702c4a18d4164bd6075e3475a45600cc3
bakutinamari <91193380+bakutinamari@users.noreply.github.com>
2022-08-09 12:30:04 +0200

    Revert "callback"

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,8 +4,4 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
   validates :body, presence :true
-
-  def number  
-    test.questions.order(:id).ids.index(id) + 1
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ApplicationRecord
-  has_many :user_tests, dependent: :destroy 
-  has_many :tests, through :user_tests
   has_many :created_tests, class_name: "Test", foreign_key: :author_id
-  has_many :test_passages
-  has_many :tests, through: test_passages
+  has_many :user_tests, dependent: :destroy
+  has_many :tests, through :user_tests
 
   validates :name, presence :true
   validates :age, numericality : { only_integer :true }
